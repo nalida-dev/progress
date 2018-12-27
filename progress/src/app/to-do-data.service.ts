4a385973ffbebdd13f6155e5fbe102a89a0d5db3
javelinsman <javelinsman@snu.ac.kr>
2018-12-27 17:32:58 +0900

    list service

--- progress/src/app/to-do-data.service.ts
+++ progress/src/app/to-do-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +13,16 @@ export class ToDoDataService {
   constructor(
     private http: HttpClient) { }
 
-  fetchToDoData(sheet_id, sheet_name, api_key){
-    if(!this.toDoData){
-      let url = "https://sheets.googleapis.com/v4/spreadsheets/" + sheet_id + "/values/" + sheet_name + "?key=" + api_key
-      return this.http.get<any>(url)
-        .pipe(
-         tap(data => this.toDoData = data)
-        );
-    }
-    else{
-      console.log("I have it");
-      return this.toDoData;
+  fetchToDoData(sheet_id, sheet_name, api_key) {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheet_id}/values/${sheet_name}?key=${api_key}`;
+    if (this.toDoData) {
+      return of(this.toDoData);
+    } else {
+      return this.http.get<any>(url).pipe(
+        tap(data => {
+          this.toDoData = data;
+        })
+      );
     }
   }
 
